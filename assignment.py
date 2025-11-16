784e855126ba71e4fb44a48b376d147d390b8a94
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-11-16 23:37:19 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,18 +1,28 @@
 # Exercise 1
-class Stack:
-    pass
+class SongNode:
+    def __init__(self, title, artist):
+        self.title = title
+        self.artist = artist
+        self.next = None
 
 
-def decimal_to_binary(n):
-    # complete this function
-    pass
+class Playlist:
+    def __init__(self):
+        self.head = None
 
+    def add_song(self, title, artist):
+        # TODO: implement
+        pass
 
-# Exercise 2
-class Queue:
-    pass
+    def delete_song(self, title):
+        # TODO: implement
+        pass
 
+    def show_playlist(self):
+        # numbered list
+        # "1. Imagine - John Lennon\n2. Hey Jude - The Beatles"
+        pass
 
-def bank_simulation(customers):
-    #complete this function
-    pass
+    def count_songs(self):
+        # return integer count
+        pass
